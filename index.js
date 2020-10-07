9468d5aaebfc0756f01200a66b5b05227d919038
TroddenSpade <troddenspade@gmail.com>
2020-10-07 17:31:08 +0330

    updated

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const axios = require("axios");
+const fs = require("fs");
 
 const config = require("./src/config/config").get();
 
 const { App } = require("./src/models/App");
 
-const Apps = require("./src/v2.json").applist.apps;
+let rawdata = fs.readFileSync("./src/v2.json");
+let Apps = JSON.parse(rawdata).applist.apps;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE, {
@@ -14,26 +16,39 @@ mongoose.connect(config.DATABASE, {
 });
 
 const get = async () => {
-  rem = [];
   for (let a of Apps) {
     let ext = await App.exists({ steam_appid: a.appid });
-    if (!ext) {
-      await rem.push(a);
+    if (ext) {
+      await Apps.pop(a);
     }
   }
-  for (let a of rem) {
-    const res = await axios.get(
-      `https://store.steampowered.com/api/appdetails?appids=${a.appid}`
-    );
+  let data = JSON.stringify({ applist: { apps: Apps } });
+  fs.writeFileSync("./src/v2.json", data);
+  for (let a of Apps) {
+    let res = await axios
+      .get(`https://store.steampowered.com/api/appdetails?appids=${a.appid}`)
+      .catch((e) => e.response.status);
+    if (res === 429) {
+      console.log("wait for 5 min...");
+      await sleep(1000 * 60 * 5);
+      res = await axios
+        .get(`https://store.steampowered.com/api/appdetails?appids=${a.appid}`)
+        .catch((e) => e.response.status);
+    }
     let app = res.data[a.appid];
-    console.log(app, a.name);
+    console.log("saved <-", a.name);
     if (app.success) {
       const game = new App(app.data);
       game.save((err, doc) => {
-        if (err) console.log(err, "error :" + a);
+        if (err) {
+          console.log(err);
+        }
       });
     }
+    Apps.pop(a);
   }
+  data = JSON.stringify({ applist: { apps: Apps } });
+  fs.writeFileSync("./src/v2.json", data);
 };
 
 get();
@@ -44,3 +59,9 @@ get();
 // };
 
 // test();
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
